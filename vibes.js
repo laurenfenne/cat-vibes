59b3cc71fcd300a55d6e9648a54fe63838a019d9
Lauren Fenne <99105713+lfenne@users.noreply.github.com>
2022-03-01 13:59:41 +1100

    Took more audio stuff out

--- vibes.js
+++ vibes.js
@@ -10,8 +10,6 @@ btn1.addEventListener('click', function () {
 
     newVid.src="Videos/cat1.mp4";
     newColour.style.color="rgb(2, 20, 1)";
-    const aud = document.getElementById("aud").play();
-
 })
 btn2.addEventListener('click', function () {
 
