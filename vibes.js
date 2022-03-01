e7c8a8605fda36af613008309f4992bbc8e09055
Lauren Fenne <99105713+lfenne@users.noreply.github.com>
2022-03-01 12:26:39 +1100

    Got JS working for choosing different back ground vids! Also made buttons grow on hover.

--- vibes.js
+++ vibes.js
@@ -127,3 +127,32 @@ const textNodes = [
 ]
 
 startGame()*/
+
+const newVid = document.querySelector('video');
+
+const btn1 = document.getElementById('1');
+const btn2 = document.getElementById('2');
+const btn3 = document.getElementById('3');
+const btn4 = document.getElementById('4');
+
+btn1.addEventListener('click', function () {
+
+    newVid.src="Videos/cat1.mp4"
+
+})
+btn2.addEventListener('click', function () {
+
+    newVid.src="Videos/cat2.mp4"
+
+})
+btn3.addEventListener('click', function () {
+
+    newVid.src="Videos/cat3.mp4"
+
+})
+btn4.addEventListener('click', function () {
+
+    newVid.src="Videos/cat4.mp4"
+
+})
+
